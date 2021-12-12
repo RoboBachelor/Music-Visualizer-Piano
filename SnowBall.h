2d948c14ad969957360493c8261c9f20fb86ed3e
Jingyi Wang <kexuedianxue@outlook.com>
2021-12-12 18:51:22 +0800

    Change the engine of ground snowball

--- SnowBall.h
+++ SnowBall.h
@@ -19,7 +19,9 @@ public:
 
 	float keyAccel = 30.f;
 	float airResistance = 1.0f;
-	float groundResistance = 4.f;
+	float goundPMax = 500000.f;
+	float goundFFactor = 1000.f;
+	float maxAccel = 2000.f;
 
 	Piano* piano;
 
@@ -64,24 +66,42 @@ public:
 		// Gravity acceleration.
 		vy -= 1500 * period / 1000;
 
-		vx += keyStatus[3] ? keyAccel : 0;
-		vx -= keyStatus[2] ? keyAccel : 0;
-		vz -= keyStatus[0] ? keyAccel : 0;
-		vz += keyStatus[1] ? keyAccel : 0;
+		if (mode == SNOWBALL_GROUND) {
+			float pwrx = 0, pwrz = 0;
+			pwrx += keyStatus[3] ? goundPMax : 0;
+			pwrx -= keyStatus[2] ? goundPMax : 0;
+			pwrz -= keyStatus[0] ? goundPMax : 0;
+			pwrz += keyStatus[1] ? goundPMax : 0;
 
-		if (mode == SNOWBALL_GROUND && !(vx == 0 && vz == 0)) {
-			float v_square = vx * vx + vz * vz;
-			float resistance = groundResistance * v_square * period / 1000;
-			vx -= resistance * vx / v_square;
-			vz -= resistance * vz / v_square;
+			float v_mag = sqrtf(vx * vx + vz * vz);
+			float fx = 0, fz = 0;
+			if (v_mag != 0) {
+				fx = -vx / v_mag;
+				fz = -vz / v_mag;
+			}
+			float ax = (vx == 0 ? pwrx : pwrx / abs(vx)) + goundFFactor * fx;
+			float az = (vz == 0 ? pwrz : pwrz / abs(vz)) + goundFFactor * fz;
+			if (ax > maxAccel) ax = maxAccel;
+			if (ax < -maxAccel) ax = -maxAccel;
+			if (az > maxAccel) az = maxAccel;
+			if (az < -maxAccel) az = -maxAccel;
+			vx += ax * period / 1000;
+			vz += az * period / 1000;
 		}
 
-		if (mode == SNOWBALL_FLYING && !(vx == 0 && vz == 0 && vy == 0)) {
-			float v_square = vx * vx + vy * vy + vz * vz;
-			float resistance = airResistance * v_square * period / 1000;
-			vx -= resistance * vx / v_square;
-			vy -= resistance * vy / v_square;
-			vz -= resistance * vz / v_square;
+		if (mode == SNOWBALL_FLYING) {
+			vx += keyStatus[3] ? keyAccel : 0;
+			vx -= keyStatus[2] ? keyAccel : 0;
+			vz -= keyStatus[0] ? keyAccel : 0;
+			vz += keyStatus[1] ? keyAccel : 0;
+
+			if (!(vx == 0 && vz == 0 && vy == 0)) {
+				float v_square = vx * vx + vy * vy + vz * vz;
+				float resistance = airResistance * v_square * period / 1000;
+				vx -= resistance * vx / v_square;
+				vy -= resistance * vy / v_square;
+				vz -= resistance * vz / v_square;
+			}
 		}
 	}
 
